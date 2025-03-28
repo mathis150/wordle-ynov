2c7f287e0acfa1077cca600e465c08a92041cac4
Mathis LENOIR <48561648+mathis150@users.noreply.github.com>
2025-03-28 09:39:22 +0100

    Implementing environnement

---
+++ env.example.php
@@ -0,0 +1,16 @@
+<?php
+
+    class Environment {
+
+        public $ENV = array();
+
+        public function __construct() {
+            $this->ENV['DB_HOST'] = 'localhost';
+            $this->ENV['DB_USER'] = 'root';
+            $this->ENV['DB_PASS'] = '';
+            $this->ENV['DB_NAME'] = 'dbname';
+        }
+
+    }
+
+?>
