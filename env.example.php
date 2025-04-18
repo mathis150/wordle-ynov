48effa5cd7ef485acd3906ee54dd597b5d63df61
Mathis LENOIR <48561648+mathis150@users.noreply.github.com>
2025-04-18 12:09:58 +0200

    Update env example file

--- env.example.php
+++ env.example.php
@@ -5,10 +5,6 @@
         public $ENV = array();
 
         public function __construct() {
-            $this->ENV['DB_HOST'] = 'localhost';
-            $this->ENV['DB_USER'] = 'root';
-            $this->ENV['DB_PASS'] = '';
-            $this->ENV['DB_NAME'] = 'dbname';
             $this->ENV['KEY'] = "your_key";
         }
 
